3486dbbde839eb13be2ef5078a00b33e43ccda2a
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-01-08 12:58:39 +0800

    fix article desc

--- dao/articles.go
+++ dao/articles.go
@@ -4,7 +4,7 @@ import "goblog/model"
 
 func GetArticlesList(page int, limit int, where map[string]interface{}, order string) (articlesList []model.View, err error) {
 	db := MDB.Table("views").Select("views.id,views.scenes,views.title,views.click,views.created_at,views.pic,views.typeid,views.content")
-	JoinDAO := db.Where("views.scenes = ?", model.BASE_POST)
+	JoinDAO := db.Where("views.scenes = ?", model.BASE_POST).Order("created_at desc")
 	JoinDAO = JoinDAO.Limit(limit).Offset(page * limit)
 	if where != nil {
 		for k, v := range where {
