4c4b42d1271a5fa7037644aff3bf788a4d406fb7
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-01-08 16:06:04 +0800

    fix three categary show

--- controller/articles.go
+++ controller/articles.go
@@ -10,16 +10,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func articleList(c *gin.Context, tab string, tpl string, page string, where map[string]interface{}) {
+func articleList(c *gin.Context, tab string, tpl string, page string, where map[string]interface{}, typeID int) {
 	// page := c.Query("page")
 	if page == "" {
 		page = "1"
 	}
+
 	pageNum, _ := strconv.ParseInt(page, 10, 32)
 	pageNum = pageNum - 1
 	// tpsID := 3 //需要查询Aritcles分类对应id
 
-	list, _ := dao.GetArticlesList(int(pageNum), 15, where, "created_at desc")
+	list, _ := dao.GetArticlesList(int(pageNum), 15, where, "created_at desc", typeID)
 	baseinfo, _ := server.Getinfo()
 
 	c.HTML(http.StatusOK, tpl, gin.H{
@@ -35,7 +36,8 @@ func Article(c *gin.Context) {
 	where := map[string]interface{}{
 		"status = ?": 1,
 	}
-	articleList(c, "articles", "articlesPage.html", page, where)
+	articlesTypeID := 8 //写死了先，后面预读到内存，用kv获取
+	articleList(c, "articles", "articlesPage.html", page, where, articlesTypeID)
 	return
 }
 
@@ -45,7 +47,8 @@ func Events(c *gin.Context) {
 	where := map[string]interface{}{
 		"status = ?": 1,
 	}
-	articleList(c, "events", "events.html", page, where)
+	eventsTypeID := 6 //写死了先，后面预读到内存，用kv获取
+	articleList(c, "events", "events.html", page, where, eventsTypeID)
 	return
 }
 
@@ -55,6 +58,7 @@ func DaoJobs(c *gin.Context) {
 	where := map[string]interface{}{
 		"status = ?": 1,
 	}
-	articleList(c, "jobs", "jobs.html", page, where)
+	DAOJobsTypeID := 7 //写死了先，后面预读到内存，用kv获取
+	articleList(c, "jobs", "jobs.html", page, where, DAOJobsTypeID)
 	return
 }
