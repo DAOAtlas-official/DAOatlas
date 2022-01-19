a9309dd86220c6c2ec7198fd483893ca83663419
wukong-brother <tuzki@github.com>
2022-01-19 18:13:54 +0800

    新改UI相关改动_202201191813

---
+++ static/component/copyright.js
@@ -0,0 +1,11 @@
+/**
+* 版权组件
+**/
+Vue.component('copy-right', {
+	template: `
+		<div class="footer-copy border-top mt-5 pt-3 d-flex justify-content-between">
+	        <div class="text-muted f12 ">&copy2021 DAOAtlas </div>
+	        <div class="text-muted f12 ">Powered by DAOAtlas Genesis</div>
+	    </div>
+	`
+});
